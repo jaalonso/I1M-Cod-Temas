c049b1e8408452e5459c55ff6a377deacf0b8b64
Jose A. Alonso <JoseA.Alonso@gmail.com>
2015-09-05 20:24:59 +0200

    Inicio del tema 4.

---
+++ test/T4Propiedades.hs
@@ -0,0 +1,21 @@
+module T4Propiedades (tests) where
+
+import I1M.Temas.T3
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Test.Tasty.HUnit
+
+tests :: TestTree
+tests = 
+    testGroup "Propiedades del tema 3"
+      [ testGroup "ejemplos"
+         [ testCase "suma"    $ (suma (2,3))   @?= 5
+         , testCase "deCeroA" $ deCeroA 5      @?= [0,1,2,3,4,5]
+         , testCase "suma'1"  $ suma' 2 3      @?= 5
+         , testCase "suma'2"  $ (suma' 2) 3    @?= 5
+         , testCase "mult1"   $ mult 2 5 7     @?= 70 
+         , testCase "mult2"   $ (mult 2) 5 7   @?= 70
+         , testCase "mult3"   $ ((mult 2) 5) 7 @?= 70
+         , testCase "suc"     $ suc 5          @?= 6 
+         ]]
+
