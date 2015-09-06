243aa38b0f27349c1c38939056dccf8c09533b7e
Jose A. Alonso <JoseA.Alonso@gmail.com>
2015-09-06 06:58:48 +0200

    Ocultación de pred del Prelude.

--- test/T4Propiedades.hs
+++ test/T4Propiedades.hs
@@ -1,5 +1,20 @@
 module T4Propiedades (tests) where
 
+import Prelude 
+    hiding ( (&&)
+           , abs
+           , const
+           , even
+           , fst
+           , head
+           , not
+           , null
+           , pred
+           , signum
+           , snd
+           , splitAt
+           , tail)
+
 import I1M.Temas.T4
 import Test.Tasty
 import Test.Tasty.QuickCheck
@@ -10,37 +25,37 @@ tests =
     testGroup "Propiedades del tema 4"
       [ testGroup "ejemplos"
          [ testCase "isDigit1" $ isDigit '3'  @?=  True
-         , textCase "isDigit2" $ isDigit 'c'  @?=  False
-         , textCase "even1"    $ even 6  @?=  True
-         , textCase "even2"    $ even 7  @?=  False
-         , textCase "splitAt"  $ splitAt 2 [3,5,7,9,4]  @?=  ([3,5],[7,9,4])
-         , textCase "abs"      $ abs (-5)  @?=  5
-         , textCase "signum1"  $ signum (-5)  @?=  -1
-         , textCase "signum2"  $ signum 0     @?=  0
-         , textCase "signum3"  $ signum 7     @?=  1
-         , textCase "abs'"     $ abs' (-5)  @?=  5
-         , textCase "signum'1" $ signum' (-5)  @?=  -1
-         , textCase "signum'2" $ signum' 0     @?=  0
-         , textCase "signum'3" $ signum' 7     @?=  1
-         , textCase "fst"      $ fst (5,3)  @?=  5
-         , textCase "snd"      $ snd (5,3)  @?=  3
-         , textCase "null1"    $ null []     @?=  True
-         , textCase "null2"    $ null [3,2]  @?=  False
-         , textCase "head"     $ head [3,2,5]  @?=  3
-         , textCase "tail"     $ tail [3,2,5]  @?=  [2,5]
-         , textCase "pred"     $ pred 5  @?=  4
-         , textCase "suma"     $ suma 2 3  @?=  5
-         , textCase "suma'"    $ suma' 2 3  @?=  5
-         , textCase "const1"   $ const 2 3  @?=  2
-         , textCase "const2"   $ const 2 7  @?=  2
-         , textCase "const'1"  $ const' 2 3  @?=  2
-         , textCase "const'2"  $ const' 2 7  @?=  2
-         , textCase "impares"  $ impares 4  @?=  [1,3,5,7]
-         , textCase "impares'" $ impares' 4  @?=  [1,3,5,7]
-         , textCase "suma''"   $ suma'' 2 3  @?=  5
-         , textCase "siguiente" $ siguiente 3  @?=  4
-         , textCase "inverso"  $ inverso 5  @?=  0.2
-         , textCase "doble"    $ doble 3  @?=  6 
-         , textCase "mitad"    $ mitad 6  @?=  3.0
+         , testCase "isDigit2" $ isDigit 'c'  @?=  False
+         , testCase "even1"    $ even 6  @?=  True
+         , testCase "even2"    $ even 7  @?=  False
+         , testCase "splitAt"  $ splitAt 2 [3,5,7,9,4]  @?=  ([3,5],[7,9,4])
+         , testCase "abs"      $ abs (-5)  @?=  5
+         , testCase "signum1"  $ signum (-5)  @?=  -1
+         , testCase "signum2"  $ signum 0     @?=  0
+         , testCase "signum3"  $ signum 7     @?=  1
+         , testCase "abs'"     $ abs' (-5)  @?=  5
+         , testCase "signum'1" $ signum' (-5)  @?=  -1
+         , testCase "signum'2" $ signum' 0     @?=  0
+         , testCase "signum'3" $ signum' 7     @?=  1
+         , testCase "fst"      $ fst (5,3)  @?=  5
+         , testCase "snd"      $ snd (5,3)  @?=  3
+         , testCase "null1"    $ null []     @?=  True
+         , testCase "null2"    $ null [3,2]  @?=  False
+         , testCase "head"     $ head [3,2,5]  @?=  3
+         , testCase "tail"     $ tail [3,2,5]  @?=  [2,5]
+         , testCase "pred"     $ pred 5  @?=  4
+         , testCase "suma"     $ suma 2 3  @?=  5
+         , testCase "suma'"    $ suma' 2 3  @?=  5
+         , testCase "const1"   $ const 2 3  @?=  2
+         , testCase "const2"   $ const 2 7  @?=  2
+         , testCase "const'1"  $ const' 2 3  @?=  2
+         , testCase "const'2"  $ const' 2 7  @?=  2
+         , testCase "impares"  $ impares 4  @?=  [1,3,5,7]
+         , testCase "impares'" $ impares' 4  @?=  [1,3,5,7]
+         , testCase "suma''"   $ suma'' 2 3  @?=  5
+         , testCase "siguiente" $ siguiente 3  @?=  4
+         , testCase "inverso"  $ inverso 5  @?=  0.2
+         , testCase "doble"    $ doble 3  @?=  6 
+         , testCase "mitad"    $ mitad 6  @?=  3.0
          ]]
 
