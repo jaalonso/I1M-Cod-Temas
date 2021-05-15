931a12610302efc8b72bc4547590d16951aa896d
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-05-15 19:16:27 +0200

    Tema_21d_PolPropiedades_Spec.hs: Propiedades de polinomios

---
+++ test/Tema_21d_PolPropiedades_Spec.hs
@@ -0,0 +1,24 @@
+module Tema_21d_PolPropiedades_Spec (main, spec) where
+
+import Tema_21.PolPropiedades
+import Test.Hspec
+import Test.QuickCheck
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "show" $ do
+    it "p1" $
+      property prop_polCero_es_cero
+    it "p2" $
+      property prop_consPol_no_cero
+    it "p3" $
+      property prop_consPol
+    it "p4" $
+      property prop_grado
+    it "p5" $
+      property prop_coefLider
+    it "p6" $
+      property prop_restoPol
