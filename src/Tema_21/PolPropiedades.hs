dc495abd6f67c9357a50972adfaa53d78b09414c
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-05-15 19:11:39 +0200

    PolPropiedades.hs: Cambio en la documentación.

--- src/Tema_21/PolPropiedades.hs
+++ src/Tema_21/PolPropiedades.hs
@@ -1,17 +1,19 @@
 -- PolPropiedades.hs
 -- Propiedades del TAD de los polinomios.
--- José A. Alonso Jiménez <jalonso@us.es>
--- Sevilla, 8 de Enero de 2011
--- ---------------------------------------------------------------------
+-- José A. Alonso Jiménez https://jaalonso.github.com
+-- =====================================================================
 
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module PolPropiedades where
+module Tema_21.PolPropiedades where
 
 -- Nota: Hay que elegir una implementación del TAD de polinomios
-import PolRepTDA
--- import PolRepDispersa
--- import PolRepDensa
+import Tema_21.PolRepTDA
+-- import Tema_21.PolRepDensa
+-- import Tema_21.PolRepDispersa
+-- import I1M.Pol
 
 import Test.QuickCheck
 
@@ -20,7 +22,7 @@ import Test.QuickCheck
 -- ---------------------------------------------------------------------
 
 -- (genPol n) es un generador de polinomios. Por ejemplo,
---    ghci> sample (genPol 1)
+--    λ> sample (genPol 1)
 --    7*x^9 + 9*x^8 + 10*x^7 + -14*x^5 + -15*x^2 + -10
 --    -4*x^8 + 2*x
 --    -8*x^9 + 4*x^8 + 2*x^6 + 4*x^5 + -6*x^4 + 5*x^2 + -8*x
@@ -38,7 +40,7 @@ genPol _ = do
   n <- choose (0,10)
   b <- choose (-10,10)
   p <- genPol (div n 2)
-  return (consPol n b p) 
+  return (consPol n b p)
 
 instance Arbitrary (Polinomio Int) where
   arbitrary = sized genPol
@@ -53,18 +55,18 @@ prop_polCero_es_cero =
   esPolCero polCero
 
 -- Comprobación.
---    ghci> quickCheck prop_polCero_es_cero
+--    λ> quickCheck prop_polCero_es_cero
 --    +++ OK, passed 100 tests.
 
--- Propiedad. Si n es mayor que el grado de p y b no es cero, entonces 
+-- Propiedad. Si n es mayor que el grado de p y b no es cero, entonces
 -- (consPol n b p) es un polinomio distinto del cero.
 prop_consPol_no_cero :: Int -> Int -> Polinomio Int -> Property
 prop_consPol_no_cero n b p =
-  n > grado p && b /= 0  ==>  
+  n > grado p && b /= 0  ==>
   not (esPolCero (consPol n b p))
 
 -- Comprobación.
---    ghci> quickCheck prop_consPol_no_cero
+--    λ> quickCheck prop_consPol_no_cero
 --    +++ OK, passed 100 tests.
 
 -- Propiedad. (consPol (grado p) (coefLider p) (restoPol p)) es igual a p.
@@ -73,39 +75,69 @@ prop_consPol p =
   consPol (grado p) (coefLider p) (restoPol p) == p
 
 -- Comprobación
---    > quickCheck prop_consPol
+--    λ> quickCheck prop_consPol
 --    +++ OK, passed 100 tests.
 
 -- Propiedad. Si n es mayor que el grado de p y b no es cero, entonces
 -- el grado de (consPol n b p) es n.
 prop_grado :: Int -> Int -> Polinomio Int -> Property
-prop_grado n b p = 
-  n > grado p && b /= 0 ==> 
-  grado (consPol n b p) == n 
-    
+prop_grado n b p =
+  n > grado p && b /= 0 ==>
+  grado (consPol n b p) == n
+
 -- Comprobación.
---    ghci> quickCheck prop_grado
+--    λ> quickCheck prop_grado
 --    +++ OK, passed 100 tests.
 
 -- Propiedad. Si n es mayor que el grado de p y b no es cero, entonces
 -- el coeficiente líder de (consPol n b p) es b.
 prop_coefLider :: Int -> Int -> Polinomio Int -> Property
-prop_coefLider n b p = 
-    n > grado p && b /= 0 ==> 
-      coefLider (consPol n b p) == b 
-    
+prop_coefLider n b p =
+  n > grado p && b /= 0 ==>
+  coefLider (consPol n b p) == b
+
 -- Comprobación.
---    ghci> quickCheck prop_coefLider
+--    λ> quickCheck prop_coefLider
 --    +++ OK, passed 100 tests.
 
 -- Propiedad. Si n es mayor que el grado de p y b no es cero, entonces
 -- el resto de (consPol n b p) es p.
 prop_restoPol :: Int -> Int -> Polinomio Int -> Property
-prop_restoPol n b p = 
-  n > grado p && b /= 0 ==> 
-  restoPol (consPol n b p) == p 
-    
+prop_restoPol n b p =
+  n > grado p && b /= 0 ==>
+  restoPol (consPol n b p) == p
+
 -- Comprobación.
---    ghci> quickCheck prop_restoPol
+--    λ> quickCheck prop_restoPol
 --    +++ OK, passed 100 tests.
 
+-- ---------------------------------------------------------------------
+-- § Verificación                                                     --
+-- ---------------------------------------------------------------------
+
+return []
+
+verificaPol :: IO Bool
+verificaPol = $quickCheckAll
+
+-- La verificación es
+--    λ> verificaPol
+--    === prop_polCero_es_cero from PolPropiedades.hs:53 ===
+--    +++ OK, passed 1 test.
+--
+--    === prop_consPol_no_cero from PolPropiedades.hs:63 ===
+--    +++ OK, passed 100 tests; 251 discarded.
+--
+--    === prop_consPol from PolPropiedades.hs:73 ===
+--    +++ OK, passed 100 tests.
+--
+--    === prop_grado from PolPropiedades.hs:83 ===
+--    +++ OK, passed 100 tests; 321 discarded.
+--
+--    === prop_coefLider from PolPropiedades.hs:94 ===
+--    +++ OK, passed 100 tests; 340 discarded.
+--
+--    === prop_restoPol from PolPropiedades.hs:105 ===
+--    +++ OK, passed 100 tests; 268 discarded.
+--
+--    True
